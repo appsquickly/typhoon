4000ae6a07d64aea6e743cc106b2047e642cecf9
Robert Gilliam <robert@robertgilliam.org>
2013-07-26 10:41:01 -0700

    Fix code coverage generation.

---
+++ Tests/Pods/Pods-ios-iOS Tests (Cocoapods)-OCMockito-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ios_iOS_Tests__Cocoapods__OCMockito : NSObject
+@end
+@implementation PodsDummy_Pods_ios_iOS_Tests__Cocoapods__OCMockito
+@end
