a222064d08362a6ce7fbb952c3690990828b9890
Jasper Blues <jasper@apssquick.ly>
2013-09-15 13:59:06 +0800

    add tests.

---
+++ Tests/Utils/TyphoonTestUtilsTests.m
@@ -0,0 +1,26 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+//  TYPHOON FRAMEWORK
+//  Copyright 2013, Jasper Blues & Contributors
+//  All Rights Reserved.
+//
+//  NOTICE: The authors permit you to use, modify, and distribute this file
+//  in accordance with the terms of the license agreement accompanying it.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface TyphoonTestUtilsTests : SenTestCase
+
+@end
+
+@implementation TyphoonTestUtilsTests
+
+- (void)test_should_waitForCondition_to_occur
+{
+
+
+}
+
+@end
