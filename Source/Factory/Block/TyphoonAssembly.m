7e5e46ca976474862246c5b8d0e841459056c31a
Jasper Blues <jasper@apssquick.ly>
2013-11-12 00:12:53 +0800

    Remove noisy loggin.

--- Source/Factory/Block/TyphoonAssembly.m
+++ Source/Factory/Block/TyphoonAssembly.m
@@ -143,7 +143,7 @@ static NSMutableArray* assemblyProperties;
         NSString* top = [resolveStack objectAtIndex:[resolveStack count] - 1];
         if ([top isEqualToString:bottom])
         {
-            LogInfo(@"Circular dependency detected in definition for key '%@'. Breaking the cycle.", key);
+            LogTrace(@"Circular dependency detected in definition for key '%@'. Breaking the cycle.", key);
             return YES;
         }
     }
