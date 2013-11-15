ce4e4e81cbd59c4ffbb37cf22d1c4d3d5ea99344
Robert Gilliam <robert@robertgilliam.org>
2013-11-15 14:32:08 -0800

    Add ComponentFactoryTests directly exercising definition inheritance.

---
+++ Tests/Factory/ClassWithConstructor.h
@@ -0,0 +1,17 @@
+//
+// Created by Robert Gilliam on 11/15/13.
+//
+
+
+#import <Foundation/Foundation.h>
+
+
+@interface ClassWithConstructor : NSObject
+
++ (instancetype)constructorWithString:(NSString*)string;
+
+- (instancetype)initWithString:(NSString*)string;
+
+@property(readonly, nonatomic) NSString *string;
+
+@end
