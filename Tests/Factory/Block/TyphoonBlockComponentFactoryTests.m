8baa326146854b8057fb6b2a63aaa296510062ca
Robert Gilliam <robert@robertgilliam.org>
2013-11-26 14:48:42 -0800

    Add test for initializing a collection of assemblies in any order.

--- Tests/Factory/Block/TyphoonBlockComponentFactoryTests.m
+++ Tests/Factory/Block/TyphoonBlockComponentFactoryTests.m
@@ -73,6 +73,20 @@
     Knight* knight = [(CollaboratingMiddleAgesAssembly*) factory knightWithExternalQuest];
     LogDebug(@"Knight: %@", knight);
     assertThat(knight, notNilValue());
+    assertThat(knight.quest, instanceOf([CampaignQuest class]));
+}
+
+- (void)test_allows_initialization_with_a_collection_of_assemblies_in_any_order
+{
+    TyphoonComponentFactory* factory = [[TyphoonBlockComponentFactory alloc] initWithAssemblies:@[
+            [CollaboratingMiddleAgesAssembly assembly],
+            [MiddleAgesAssembly assembly]
+    ]];
+
+    Knight* knight = [(CollaboratingMiddleAgesAssembly*) factory knightWithExternalQuest];
+    LogDebug(@"Knight: %@", knight);
+    assertThat(knight, notNilValue());
+    assertThat(knight.quest, instanceOf([CampaignQuest class]));
 }
 
 - (void)test_allows_overriding_methods_in_an_assembly
