40233745834b9c9cf6a54fce171f112cf8094534
Jasper Blues <jasper@appsquick.ly>
2013-11-29 12:41:41 +0800

    Add to umbrella header.

--- Source/Typhoon.h
+++ Source/Typhoon.h
@@ -26,6 +26,7 @@
 #import "TyphoonIntrospectionUtils.h"
 #import "TyphoonAssembly.h"
 #import "TyphoonCollaboratingAssemblyProxy.h"
+#import "TyphoonPropertyInjectedAsCollection.h"
 
 #import "TyphoonBlockComponentFactory.h"
 
