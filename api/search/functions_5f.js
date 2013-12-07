3866ba236e4be3012c8d26eecd52ca71cb515f3a
Jasper Blues <jasper@appsquick.ly>
2013-12-07 14:05:07 +0800

    publish reports to gh-pages

--- api/search/functions_5f.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['_5fdummygetter',['_dummyGetter',['../interface_typhoon_assisted_factory_base.html#a5f49f363509789fb17c084694a7996d3',1,'TyphoonAssistedFactoryBase']]],
-  ['_5ffmc_5fclosureforselector_3a',['_fmc_closureForSelector:',['../category_typhoon_assisted_factory_base_07_typhoon_factory_method_closure_08.html#a2ace3a0bc1025fc34d6b14f2327885bd',1,'TyphoonAssistedFactoryBase(TyphoonFactoryMethodClosure)::_fmc_closureForSelector:()'],['../interface_typhoon_assisted_factory_base.html#a2ace3a0bc1025fc34d6b14f2327885bd',1,'TyphoonAssistedFactoryBase::_fmc_closureForSelector:()']]],
-  ['_5ffmc_5fsetclosure_3aforselector_3a',['_fmc_setClosure:forSelector:',['../category_typhoon_assisted_factory_base_07_typhoon_factory_method_closure_08.html#ae653e97c999bf3f84f27f278b9302b36',1,'TyphoonAssistedFactoryBase(TyphoonFactoryMethodClosure)::_fmc_setClosure:forSelector:()'],['../interface_typhoon_assisted_factory_base.html#ae653e97c999bf3f84f27f278b9302b36',1,'TyphoonAssistedFactoryBase::_fmc_setClosure:forSelector:()']]],
-  ['_5fsetdummysetter_3a',['_setDummySetter:',['../interface_typhoon_assisted_factory_base.html#a4981c8daf962ac6d790f899e635c7bd7',1,'TyphoonAssistedFactoryBase']]]
-];
