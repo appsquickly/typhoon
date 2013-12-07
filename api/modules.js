225f7f87880ae263c1d57aa646c09b0b73526d17
Jasper Blues <jasper@appsquick.ly>
2013-12-07 15:13:02 +0800

    publish reports to gh-pages

--- api/modules.js
+++ api/modules.js
@@ -1,7 +1,7 @@
 var modules =
 [
-    [ "Configuration", "group___configuration.html", "group___configuration" ],
-    [ "Definition", "group___definition.html", "group___definition" ],
     [ "Factory", "group___factory.html", "group___factory" ],
+    [ "Definition", "group___definition.html", "group___definition" ],
+    [ "Configuration", "group___configuration.html", "group___configuration" ],
     [ "Test", "group___test.html", "group___test" ]
 ];
