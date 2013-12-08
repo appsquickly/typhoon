b519b11cf8c7c82d4878c523933c044964ac6fff
Jasper Blues <jasper@appsquick.ly>
2013-12-08 15:03:54 +0800

    Move some files to 'Internal' group.

---
+++ Source/Factory/Internal/TyphoonComponentFactory+TyphoonDefinitionRegisterer.h
@@ -0,0 +1,25 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+//  TYPHOON FRAMEWORK
+//  Copyright 2013, Jasper Blues & Contributors
+//  All Rights Reserved.
+//
+//  NOTICE: The authors permit you to use, modify, and distribute this file
+//  in accordance with the terms of the license agreement accompanying it.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+
+
+#import <Foundation/Foundation.h>
+#import "TyphoonComponentFactory.h"
+
+@interface TyphoonComponentFactory (TyphoonDefinitionRegisterer)
+
+- (TyphoonDefinition*)definitionForKey:(NSString*)key;
+
+- (id)objectForDefinition:(TyphoonDefinition*)definition;
+
+- (void)addDefinitionToRegistry:(TyphoonDefinition*)definition;
+
+@end
