a66e49083abd7411a6f07140cf4322c266eeb314
Jasper Blues <jasper@appsquick.ly>
2014-01-30 10:06:16 +0800

    Update header.

--- Source/Factory/Pool/TyphoonComponentsPool.h
+++ Source/Factory/Pool/TyphoonComponentsPool.h
@@ -1,10 +1,14 @@
+////////////////////////////////////////////////////////////////////////////////
 //
-//  TyphoonComponentsPool.h
-//  A-Typhoon
+//  TYPHOON FRAMEWORK
+//  Copyright 2013, Jasper Blues & Contributors
+//  All Rights Reserved.
 //
-//  Created by Aleksey Garbarev on 29.01.14.
-//  Copyright (c) 2014 Jasper Blues. All rights reserved.
+//  NOTICE: The authors permit you to use, modify, and distribute this file
+//  in accordance with the terms of the license agreement accompanying it.
 //
+////////////////////////////////////////////////////////////////////////////////
+
 
 #import <Foundation/Foundation.h>
 
