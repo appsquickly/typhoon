ea1011513e381519da3a90339fde839271863bfc
Jasper Blues <jasper@appsquick.ly>
2014-05-29 14:30:35 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
-rm -fr build
-rm -fr ~/Library/Developer/Xcode/DerivedData
-xcodebuild -project Typhoon.xcodeproj/ -scheme 'iOS Tests' -configuration Debug -destination OS=7.1,name=iPad test
 
 rm -fr build
 rm -fr ~/Library/Developer/Xcode/DerivedData
 xcodebuild -project Typhoon.xcodeproj/ -scheme 'Typhoon-OSXTests' test
+
+
+rm -fr build
+rm -fr ~/Library/Developer/Xcode/DerivedData
+xcodebuild -project Typhoon.xcodeproj/ -scheme 'Typhoon-iOS' -configuration Debug -destination OS=7.1,name=iPad test
+
 
