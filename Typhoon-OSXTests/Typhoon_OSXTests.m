45a8d070c1d0259f3065f164189415a7b46db7cd
Jasper Blues <jasper@appsquick.ly>
2014-05-29 14:27:08 +0800

    Tests on iOS (device / sim) and OSX.

---
+++ Typhoon-OSXTests/Typhoon_OSXTests.m
@@ -0,0 +1,34 @@
+//
+//  Typhoon_OSXTests.m
+//  Typhoon-OSXTests
+//
+//  Created by Jasper Blues on 5/29/14.
+//  Copyright (c) 2014 typhoonframework.org. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Typhoon_OSXTests : XCTestCase
+
+@end
+
+@implementation Typhoon_OSXTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
--- build.sh
+++ build.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+rm -fr build
+rm -fr ~/Library/Developer/Xcode/DerivedData
 xcodebuild -project Typhoon.xcodeproj/ -scheme 'iOS Tests' -configuration Debug -destination OS=7.1,name=iPad test
+
+rm -fr build
+rm -fr ~/Library/Developer/Xcode/DerivedData
 xcodebuild -project Typhoon.xcodeproj/ -scheme 'Typhoon-OSXTests' test
 
