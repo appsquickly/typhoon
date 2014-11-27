49e28d43705a57a72b094b172d0553a2198d9da1
Jasper Blues <jasper@appsquick.ly>
2014-11-27 13:42:53 +0800

    publish reports to gh-pages

--- api/category_typhoon_definition_07_deprecated_08.js
+++ api/category_typhoon_definition_07_deprecated_08.js
@@ -1,5 +1,5 @@
 var category_typhoon_definition_07_deprecated_08 =
 [
-    [ "setAfterInjections:", "category_typhoon_definition_07_deprecated_08.html#a45d5caae23921b2a5cf2c84b3c093d82", null ],
-    [ "setBeforeInjections:", "category_typhoon_definition_07_deprecated_08.html#a2c2a25760353ea2c3402dad9c02bf339", null ]
+    [ "setAfterInjections:", "category_typhoon_definition_07_deprecated_08.html#a57429df9ce05390bbf2e743dd9f90e3e", null ],
+    [ "setBeforeInjections:", "category_typhoon_definition_07_deprecated_08.html#a669f26a402157077c7ae205a65ee9613", null ]
 ];
