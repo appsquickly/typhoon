e3f1c7d85cc9b47c561737fca19fb2b4941b89fd
Jasper Blues <jasper@appsquick.ly>
2014-12-09 14:49:21 +0800

    Apply PR from @mowens.

--- Source/Configuration/ConfigPostProcessor/TyphoonConfiguration/TyphoonPlistStyleConfiguration.m
+++ Source/Configuration/ConfigPostProcessor/TyphoonConfiguration/TyphoonPlistStyleConfiguration.m
@@ -1,7 +1,13 @@
+////////////////////////////////////////////////////////////////////////////////
 //
-// Created by Aleksey Garbarev on 27.05.14.
-// Copyright (c) 2014 Jasper Blues. All rights reserved.
+//  TYPHOON FRAMEWORK
+//  Copyright 2013, Jasper Blues & Contributors
+//  All Rights Reserved.
 //
+//  NOTICE: The authors permit you to use, modify, and distribute this file
+//  in accordance with the terms of the license agreement accompanying it.
+//
+////////////////////////////////////////////////////////////////////////////////
 
 #import "TyphoonPlistStyleConfiguration.h"
 #import "TyphoonResource.h"
@@ -9,37 +15,55 @@
 
 @implementation TyphoonPlistStyleConfiguration
 {
-    NSMutableDictionary *_properties;
+    NSMutableDictionary* _properties;
 }
 
 - (id)init
 {
     self = [super init];
-    if (self) {
+    if (self)
+    {
         _properties = [NSMutableDictionary new];
     }
     return self;
 }
 
-- (void)appendResource:(id<TyphoonResource>)resource
+- (void)appendResource:(id <TyphoonResource>)resource
 {
-    NSError *error = nil;
-    NSDictionary *dictionary = [NSPropertyListSerialization propertyListWithData:[resource data]
-                                                                         options:NSPropertyListImmutable
-                                                                          format:NULL
-                                                                           error:&error];
-    if (![dictionary isKindOfClass:[NSDictionary class]]) {
+    NSString* errorString = nil;
+    NSDictionary* dictionary = nil;
+
+    // remove deprecated warning when targeting iOS 8 + and OSX 10.6 +
+#if (TARGET_OS_IPHONE && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_8) || (TARGET_OS_MAC && MAC_OS_X_VERSION_MIN_REQUIRED >= MAC_OS_X_VERSION_10_10)
+    NSError* error = nil;
+    dictionary =
+        [NSPropertyListSerialization propertyListWithData:[resource data] options:NSPropertyListImmutable format:NULL error:&error];
+    if (error != nil)
+    {
+        errorString = [error localizedDescription];
+    }
+#else
+    dictionary = [NSPropertyListSerialization propertyListFromData:[resource data]
+                                                  mutabilityOption:NSPropertyListImmutable
+                                                            format:NULL
+                                                  errorDescription:&errorString];
+#endif
+    if (![dictionary isKindOfClass:[NSDictionary class]])
+    {
         [NSException raise:NSInvalidArgumentException format:@"Root plist object must be a dictionary"];
     }
 
-    if (!error) {
+    if (!errorString)
+    {
         [_properties addEntriesFromDictionary:dictionary];
-    } else {
-        [NSException raise:NSInvalidArgumentException format:@"Can't parse plist configuration file: %@", error.localizedDescription];
+    }
+    else
+    {
+        [NSException raise:NSInvalidArgumentException format:@"Can't prase plist configuration file: %@", errorString];
     }
 }
 
-- (id)objectForKey:(NSString *)key
+- (id)objectForKey:(NSString*)key
 {
     return [_properties valueForKeyPath:key];
 }
