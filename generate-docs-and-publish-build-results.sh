7cc4ada9f6baea32b885cc3ff6a6a6b8b3d08e54
Jasper Blues <jasper@appsquick.ly>
2014-12-15 13:35:00 +0800

    Publish docs to docs/latest.

--- generate-docs-and-publish-build-results.sh
+++ generate-docs-and-publish-build-results.sh
@@ -32,7 +32,7 @@ cp -fr build/reports/build-status/build-status.png ./build-status/build-status.p
 git add build-status
 
 rm -fr ./api
-cp -fr ${reportsDir}/api ./api
+cp -fr ${reportsDir}/api ./docs/latest/api
 git add api
 
 git commit -a -m "publish reports to gh-pages" || true # allow `remote add` to fail without failing script (if nothing to add)
