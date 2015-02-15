0b4c947fbf038d8a158dc6dd04d8929886801671
Jasper Blues <jasper@appsquick.ly>
2015-02-15 08:37:18 +0800

    publish reports to gh-pages

--- docs/latest/api/group___factory.js
+++ docs/latest/api/group___factory.js
@@ -4,6 +4,21 @@ var group___factory =
       [ "assembly", "interface_typhoon_assembly.html#af53fcd1ea8b4e55dda2080a651814c6c", null ],
       [ "defaultAssembly", "interface_typhoon_assembly.html#ac445c91bdd47b3389eadedbf33860512", null ]
     ] ],
+    [ "TyphoonComponentFactory", "interface_typhoon_component_factory.html", [
+      [ "attachPostProcessor:", "interface_typhoon_component_factory.html#a497444322c5eb57d64a327f170cbd389", null ],
+      [ "defaultFactory", "interface_typhoon_component_factory.html#ad0d0eb369da7b2cd39caf403e9c91ddf", null ],
+      [ "enumerateDefinitions:", "interface_typhoon_component_factory.html#a57cb1e89e38d67d01c72cc36bf592d3a", null ],
+      [ "factoryForResolvingFromXibs", "interface_typhoon_component_factory.html#a9f9dfff1c95f8c83303889d7202df414", null ],
+      [ "load", "interface_typhoon_component_factory.html#a5682d54bbebb3e5eca92707339bee4ce", null ],
+      [ "registerDefinition:", "interface_typhoon_component_factory.html#ae4569eb6aebca4089de316d2b80d34d1", null ],
+      [ "registry", "interface_typhoon_component_factory.html#afa734936e2f650ba86b731ec67cd7ce9", null ],
+      [ "setFactoryForResolvingFromXibs:", "interface_typhoon_component_factory.html#abe5efdbe98a5d5779b952cb66e06f0f4", null ],
+      [ "unload", "interface_typhoon_component_factory.html#a810e359a13f87d375088a78759227692", null ],
+      [ "definitionPostProcessors", "interface_typhoon_component_factory.html#a1cdbafd8d079d52fd72cab4efddbbcbc", null ],
+      [ "instancePostProcessors", "interface_typhoon_component_factory.html#a6647913e4fc87992108fa703df42c970", null ],
+      [ "loaded", "interface_typhoon_component_factory.html#a09b13d5d400d8ca043cc5f1105b8a744", null ],
+      [ "singletons", "interface_typhoon_component_factory.html#a54c45340de66c65390e529f237fc5785", null ]
+    ] ],
     [ "<TyphoonComponentFactory>", "protocol_typhoon_component_factory-p.html", [
       [ "allComponentsForType:", "protocol_typhoon_component_factory-p.html#aea027bcac4e17f242ac23662ac00c6c4", null ],
       [ "componentForKey:", "protocol_typhoon_component_factory-p.html#abbbebd539b0c507d2c22bfb77e954c9c", null ],
