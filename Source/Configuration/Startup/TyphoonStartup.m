58c3f607ca955ac89aa2056044c706ea291643b5
Egor Tolstoy <e.tolstoy@rambler-co.ru>
2015-08-16 14:09:07 +0300

    Added the new way of providing initial assemblies for activating by implementing the method -initialAssemblies in the AppDelegate

--- Source/Configuration/Startup/TyphoonStartup.m
+++ Source/Configuration/Startup/TyphoonStartup.m
@@ -17,7 +17,7 @@
 #import "TyphoonIntrospectionUtils.h"
 #import "TyphoonConfigPostProcessor.h"
 #import "OCLogTemplate.h"
-
+#import "TyphoonAssemblyBuilder+PlistProcessor.h"
 
 #import <objc/runtime.h>
 
@@ -44,11 +44,22 @@
 + (TyphoonComponentFactory *)factoryFromAppDelegate:(id)appDelegate
 {
     TyphoonComponentFactory *result = nil;
-
-    if ([appDelegate respondsToSelector:@selector(initialFactory)]) {
-        result = [appDelegate initialFactory];
+    SEL initialFactorySelector = NSSelectorFromString(@"initialFactory");
+    SEL initialAssembliesSelector = NSSelectorFromString(@"initialAssemblies");
+    
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Warc-performSelector-leaks"
+    if ([appDelegate respondsToSelector:initialFactorySelector]) {
+        result = [appDelegate performSelector:initialFactorySelector];
     }
-
+    
+    if ([appDelegate respondsToSelector:initialAssembliesSelector]) {
+        NSArray *assemblyClasses = [appDelegate performSelector:initialAssembliesSelector];
+        NSArray *assemblies = [TyphoonAssemblyBuilder buildAssembliesWithClasses:assemblyClasses];
+        result = [TyphoonBlockComponentFactory factoryWithAssemblies:assemblies];
+    }
+#pragma clang diagnostic pop
+    
     return result;
 }
 
