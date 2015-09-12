a4708fc389191841c52432905f766774c5cad6a9
Egor Tolstoy <e.tolstoy@rambler-co.ru>
2015-09-12 11:20:17 +0300

    Added TyphoonStoryboardProvider - an object that collects the list of assemblies from the provided bundle and filters them using the data from Info.plist file

---
+++ Source/ios/Storyboard/TyphoonStoryboardProvider.h
@@ -0,0 +1,18 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+//  TYPHOON FRAMEWORK
+//  Copyright 2015, Typhoon Framework Contributors
+//  All Rights Reserved.
+//
+//  NOTICE: The authors permit you to use, modify, and distribute this file
+//  in accordance with the terms of the license agreement accompanying it.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#import <Foundation/Foundation.h>
+
+@interface TyphoonStoryboardProvider : NSObject
+
+- (NSArray *)collectStoryboardsFromBundle:(NSBundle *)bundle;
+
+@end
