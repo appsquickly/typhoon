dc357819b905d3e5c02947329844b253b7e86758
Anton Pomozov <pomozov@shtrih-m.ru>
2015-09-16 17:19:46 +0300

    Add #pragma to avoid warnings about "Depricated usages and implementations"

--- Source/Factory/Assembly/TyphoonAssemblyActivator.m
+++ Source/Factory/Assembly/TyphoonAssemblyActivator.m
@@ -20,6 +20,8 @@
 
 @end
 
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-implementations"
 
 @implementation TyphoonAssemblyActivator {
     NSArray *_assemblies;
@@ -60,5 +62,6 @@
     }
 }
 
-
 @end
+
+#pragma clang diagnostic pop
