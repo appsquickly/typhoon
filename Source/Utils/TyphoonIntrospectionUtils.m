23ff7cf2009d50ef77c0095a67cefb49e92b6329
Aleksei Shevchenko <aleksei.shevchenko@bluechipsys.com>
2015-10-12 16:26:42 -0700

    Another iOS-specific TyphoonClassFromString performance optimization

--- Source/Utils/TyphoonIntrospectionUtils.m
+++ Source/Utils/TyphoonIntrospectionUtils.m
@@ -308,12 +308,31 @@ Class TyphoonClassFromString(NSString *className)
             return nil;
         }
         
-        NSArray *frameworks = [NSBundle allFrameworks];
-        for (uint i = 0; i < frameworks.count && clazz == nil; ++i) {
-            NSBundle *framework = [frameworks objectAtIndex:i];
-            NSString *bundleIdentifier = [framework bundleIdentifier];
-            // ignore apple frameworks
-            if (![bundleIdentifier hasPrefix:@"com.apple"]) {
+        #if TARGET_OS_IOS
+        // on iOS, we can safely precache frameworks
+        // because they'll never change during app lifetime
+        {
+            static NSArray * frameworkNames = nil;
+            static dispatch_once_t onceToken;
+            dispatch_once(&onceToken, ^{
+                NSMutableArray * temporaryNames = [NSMutableArray array];
+                
+                NSArray *frameworks = [NSBundle allFrameworks];
+                for (uint i = 0; i < frameworks.count; ++i) {
+                    NSBundle *framework = [frameworks objectAtIndex:i];
+                    NSString *bundleIdentifier = [framework bundleIdentifier];
+                    // ignore apple frameworks and resource bundles
+                    if (bundleIdentifier && ![bundleIdentifier hasPrefix:@"com.apple"]) {
+                        [temporaryNames addObject:bundleIdentifier];
+                    }
+                }
+                frameworkNames = [temporaryNames copy];
+            });
+            
+            for (uint i = 0; i < frameworkNames.count && clazz == nil; ++i) {
+                
+                NSString *bundleIdentifier = frameworkNames[i];
+                
                 NSRange range = [bundleIdentifier rangeOfString:@"." options:NSBackwardsSearch];
                 if (range.location != NSNotFound) {
                     NSString *frameworkName = [bundleIdentifier substringFromIndex:range.location + 1];
@@ -323,6 +342,29 @@ Class TyphoonClassFromString(NSString *className)
                 }
             }
         }
+        #else
+        // For OSX we still need to iterate, since platform allows us to use downloaded frameworks
+        // and they can change during app lifetime
+        {
+            NSArray *frameworks = [NSBundle allFrameworks];
+            for (uint i = 0; i < frameworks.count && clazz == nil; ++i) {
+                NSBundle *framework = [frameworks objectAtIndex:i];
+                NSString *bundleIdentifier = [framework bundleIdentifier];
+                // ignore apple frameworks
+                if (![bundleIdentifier hasPrefix:@"com.apple"]) {
+                    NSRange range = [bundleIdentifier rangeOfString:@"." options:NSBackwardsSearch];
+                    if (range.location != NSNotFound) {
+                        NSString *frameworkName = [bundleIdentifier substringFromIndex:range.location + 1];
+                        if (frameworkName != nil) {
+                            clazz = NSClassFromString([frameworkName stringByAppendingFormat:@".%@", className]);
+                        }
+                    }
+                }
+            }
+        }
+        #endif
+        
+        
     }
     return clazz;
 }
