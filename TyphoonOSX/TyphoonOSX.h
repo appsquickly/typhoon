1d704632f9a48c8e1df5e234365dcaf6d345f4ee
József Vesza <jozsefvesza@Jozsef-Veszas-MacBook-Pro.local>
2016-03-26 17:47:47 +0100

    #459: Added Cocoa Framework target.

---
+++ TyphoonOSX/TyphoonOSX.h
@@ -0,0 +1,22 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+//  TYPHOON FRAMEWORK
+//  Copyright 2013, Typhoon Framework Contributors
+//  All Rights Reserved.
+//
+//  NOTICE: The authors permit you to use, modify, and distribute this file
+//  in accordance with the terms of the license agreement accompanying it.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for TyphoonOSX.
+FOUNDATION_EXPORT double TyphoonOSXVersionNumber;
+
+//! Project version string for TyphoonOSX.
+FOUNDATION_EXPORT const unsigned char TyphoonOSXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TyphoonOSX/PublicHeader.h>
+
+
