5867b420c80b4370c9475b39a3f84aad8acf35fb
Aleksey Garbarev <aleksey@appsquick.ly>
2016-10-19 13:50:58 +0600

    travisbuild.sh updated

--- travisbuild.sh
+++ travisbuild.sh
@@ -2,7 +2,7 @@
 set -e
 set -o pipefail
 
-xcodebuild clean test -project Typhoon.xcodeproj -scheme 'Typhoon-iOSTests' -configuration Debug \
--destination "platform=iOS Simulator,name=iPhone 5s,OS=latest"
+xcodebuild test -project Typhoon.xcodeproj -scheme 'Typhoon-iOSTests' -configuration Debug \
+-destination "platform=iOS Simulator,name=iPhone 6,OS=latest" ONLY_ACTIVE_ARCH=NO CODE_SIGNING_REQUIRED=NO CODE_SIGN_IDENTITY=""
 
 #xcodebuild -project Typhoon.xcodeproj/ -scheme 'Typhoon-OSXTests' clean test | xcpretty -c --report junit
