584f7fd3c139c43fd91c1445892256f85811e36e
Aleksey Garbarev <alex.garbarev@gmail.com>
2017-07-14 16:53:31 +0600

    Trying to run tests with fastlane

--- travisbuild.sh
+++ travisbuild.sh
@@ -1,8 +1,10 @@
 #Fail immediately if a task fails
-set -e
-set -o pipefail
+#set -e
+#set -o pipefail
 
-xcodebuild test -project Typhoon.xcodeproj -scheme 'Typhoon-iOSTests' -configuration Debug \
--destination "platform=iOS Simulator,name=iPhone 6,OS=latest" ONLY_ACTIVE_ARCH=NO CODE_SIGNING_REQUIRED=NO CODE_SIGN_IDENTITY=""
+fastlane scan Typhoon.xcodeproj --scheme Typhoon-iOSTests
+
+#xcodebuild test -project Typhoon.xcodeproj -scheme 'Typhoon-iOSTests' -configuration Debug \
+#-destination "platform=iOS Simulator,name=iPhone 6,OS=latest" ONLY_ACTIVE_ARCH=NO CODE_SIGNING_REQUIRED=NO CODE_SIGN_IDENTITY=""
 
 #xcodebuild -project Typhoon.xcodeproj/ -scheme 'Typhoon-OSXTests' clean test | xcpretty -c --report junit
